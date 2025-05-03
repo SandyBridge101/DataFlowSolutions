7258f94edfcfc146c83bee0e867b053487e135a0
Tariq Nasser Deen <tariqnassiru@gmail.com>
2025-05-03 00:04:27 +0000

    chore: Implement 'Main' class

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,10 @@
 package main;
-
 import static javafx.application.Application.launch;
 import ui.MainApp;
 
 
-
 public class Main {
     public static void main(String[] args) {
-        launch(MainApp.class,args);
+        launch(MainApp.class,args);//launch UI application
     }
 }
